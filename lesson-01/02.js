5d48f5ee48ddc607f2054f4ec4cd12c211a11d72
Vladislav Loginov <loginov.vlados@yandex.ru>
2023-11-07 18:20:34 +0300

    odd or even refactor

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,5 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
 console.log(
-  number1 % 2 === 0 ? number1 + " Чётное число" : number1 + " Нечётное число"
+  number1 % 2 === 0 ? number1 + "чётное число" : number1 + "нечётное число"
 );
