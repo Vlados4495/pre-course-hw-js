e896d08415a0c3d9f0b3263e729448a929ba0288
Vladislav Loginov <loginov.vlados@yandex.ru>
2023-11-07 18:40:57 +0300

    odd or even

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,7 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
 console.log(
-  number1 % 2 === 0 ? number1 + "чётное число" : number1 + "нечётное число"
+  number1 % 2 === 0
+    ? number1 + "Это четное число"
+    : number1 + "Это нечетное число"
 );
