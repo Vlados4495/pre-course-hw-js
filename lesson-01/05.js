d2e2cd8482c0923ead8cc7602c21810791395232
Vladislav Loginov <loginov.vlados@yandex.ru>
2023-11-09 00:16:33 +0300

    fixed

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -8,6 +8,11 @@ switch (true) {
   case rule.length > 25:
     console.log('У меня 100 пудов все получится');
     break;
-  default:
+
+  case rule.length === 25:
     console.log('50 на 50');
+    break;
+
+  default:
+    console.log('Default');
 }
